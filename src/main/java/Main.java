02d9ef7bb28ee559cc9709d4689a04dcecc3396d
igorpav <Igor.Pavlichenkov@tatar.ru>
2020-05-25 02:21:56 +0300

    Final version

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final CryptoTestParser cryptoTestParser = new CryptoTestParser();
+        final List<Test> tests = cryptoTestParser.readTestDataFromFile("tests/9.2.30.Serpent.vectors256.txt");
+        final Serpent serpent = new Serpent();
+
+        int success = 0;
+        int failure = 0;
+
+        for (final Test test: tests) {
+            final String encryptedText = serpent.encrypt(test.getPlain(), test.getKey());
+            final String decryptedText = serpent.decrypt(encryptedText, test.getKey());
+            final boolean isEncryptedTextCorrect = test.getCipher().equals(encryptedText);
+            final boolean isDecryptedTextCorrect = test.getPlain().equals(decryptedText);
+            if (isDecryptedTextCorrect && isEncryptedTextCorrect) {
+                success++;
+            } else {
+                failure++;
+                System.out.println(test);
+            }
+        }
+
+        System.out.println("Success: " + success);
+        System.out.println("Failure: " + failure);
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Test {
+    private int setNumber;
+    private int vectorNumber;
+    private String key;
+    private String plain;
+    private String decrypted;
+    private String encrypted;
+    private String cipher;
+    private String hundredTimes;
+    private String thousandTimes;
+}
